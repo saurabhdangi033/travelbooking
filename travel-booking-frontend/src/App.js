a0d7e049873ce435c7a3bc84d354ea13f24ca5f0
saurabhdangi03 <104308074+saurabhdangi03@users.noreply.github.com>
2024-10-09 01:20:25 +0530

    commit

--- travel-booking-frontend/src/App.js
+++ travel-booking-frontend/src/App.js
@@ -4,9 +4,19 @@ import Navbar from './components/Navbar';
 import BookingForm from './components/BookingForm';
 import BookingList from './components/BookingList';
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://travelbooking-gamma.vercel.app/api/graphql',  // Use the Vercel deployed backend URL
+  cache: new InMemoryCache(),
+});
+
+
+
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <div className="App">
         <Navbar />
@@ -16,6 +26,7 @@ function App() {
         </Routes>
       </div>
     </Router>
+    </ApolloProvider>
   );
 }
 
