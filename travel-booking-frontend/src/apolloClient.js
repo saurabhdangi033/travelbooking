f42a5d7bd3c909cfd5feca351ef2574eac844685
saurabhdangi03 <104308074+saurabhdangi03@users.noreply.github.com>
2024-10-09 10:25:32 +0530

    commit

--- travel-booking-frontend/src/apolloClient.js
+++ travel-booking-frontend/src/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://travelbookingbackend-j6o4.onrender.com/graphql', // GraphQL endpoint
+  uri: 'https://travelbooking-gamma.vercel.app/graphql', // GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
